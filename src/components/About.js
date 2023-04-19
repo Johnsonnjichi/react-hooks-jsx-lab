ef45a31335188c6c39529c53a71d3aaec892e39e
johnson <johnsonnjichi@gmail.com>
2023-04-19 14:47:50 +0300

    Portfolio page

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,15 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+    <h2>About Me</h2>
+
+    <p>Changing the world with one line of code at a time</p>
+    <img src={image} alt="I made this"/>
+
+  </div>
+  )
 }
 
 export default About;
